ff0c42aa550f605e7cae1a6a0cf5bda3107053b3
start.vaadin.com <no-reply@vaadin.com>
2022-05-20 03:31:51 +0000

    Generated project

---
+++ src/main/java/firma/Application.java
@@ -0,0 +1,29 @@
+package firma;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "firmavaadin", variant = Lumo.DARK)
+@PWA(name = "firmavaadin", shortName = "firmavaadin", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
